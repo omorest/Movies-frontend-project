5796730851607f0a034c0c2c63afd0817c418b1a
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-03-07 22:17:40 +0000

    refactor cards and filter fetch

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,19 @@
 export const fetchMovies = async (url: string) => {
   const data = await fetch(url)
   const { results } = await data.json()
-  return results
+  const elements = results.filter((element: any) => element.poster_path !== null)
+  console.log(elements)
+  return elements
+}
+
+export const fetchCasts = async (url: string) => {
+  const data = await fetch(url)
+  const { results } = await data.json()
+  return results.filter((cast: any) => cast.profile_path !== null)
+}
+
+export const fetchCompanies = async (url: string) => {
+  const data = await fetch(url)
+  const { results } = await data.json()
+  return results.filter((company: any) => company.logo_path !== null)
 }
