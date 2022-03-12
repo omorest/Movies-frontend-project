cda9f0a25905480bc2bd13ceded3adfaee1a89ef
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-03-12 17:56:43 +0000

    vercel json

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import './App.css'
 import { Home, LoggedPage, SearchPage, DiscoverPage, FavouritesPage, DetailsPage } from './pages/'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Container } from '@chakra-ui/react'
 
 function App () {
   return (
     <Container maxW='85%'>
       <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/search' element={<SearchPage />} />
-            <Route path='/discover' element={<DiscoverPage />} />
-            <Route path='/favourites' element={<FavouritesPage />} />
-            <Route path='/logged' element={<LoggedPage />} />
-            <Route path='/details/:id/' element={<DetailsPage />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='/discover' element={<DiscoverPage />} />
+          <Route path='/favourites' element={<FavouritesPage />} />
+          <Route path='/logged' element={<LoggedPage />} />
+          <Route path='/details/:id/' element={<DetailsPage />} />
+        </Routes>
       </div>
     </Container>
   )
